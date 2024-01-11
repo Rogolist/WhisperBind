04c9d24db86250621d81c4699940c7b3612d5641
Rogolist <petro.nekhaichyk@gmail.com>
2024-01-11 15:42:02 +0100

    код переписан для динамической настройки количества строк сохранения

--- !WhisperBind/whisperbind.lua
+++ !WhisperBind/whisperbind.lua
@@ -1,169 +1,180 @@
-optvalue = 0;
+wbopt = 0
+wbcount = nil
+
+WB_buttonbox = {}
+WB_playerbox = {}
+WB_messagebox = {}
+
+--DEFAULT_CHAT_FRAME:AddMessage(saveinformation[1], 1.0, 1.0, 0.0)
 
 -- https://wowwiki-archive.fandom.com/wiki/USERAPI_SlashCmdList_AddSlashCommand
 -- /forbium
 
-WB_storage = {}
+-- Rogolist edition 2024.01.11
 
-function whisperbind_OnLoad()
+
+
+
+function wbinterface_OnLoad()
     this:RegisterEvent("ADDON_LOADED");
 
-    DEFAULT_CHAT_FRAME:AddMessage("WhisperBind is loaded", 2.0, 0.0, 1.0)
-	UIErrorsFrame:AddMessage("WhisperBind is loaded", 2.0, 0.0, 1.0, 0.0, UIERRORS_HOLD_TIME);
-    whisperbind:Show()
-    options:Hide()
-    optvalue = 0;
-
+    DEFAULT_CHAT_FRAME:AddMessage("WhisperBind is loaded", 1.0, 1.0, 0.0)
+    WBInterfaceFrame:Show()
+    --options:Hide()
+    --wbopt = 0;
+	--WBInterfaceFrame_Resize(wbcount)
+	--defaultVariables()
 end
 
+
 -- https://wowwiki-archive.fandom.com/wiki/Saving_variables_between_game_sessions
-function whisperbind_OnEvent(event)
+function wbinterface_OnEvent(event)
 	if (event == "ADDON_LOADED") then
+		--addons_massiv[iii] = ("LOADED: === "..arg1)
+		--iii = iii + 1;
+		--DEFAULT_CHAT_FRAME:AddMessage("LOADED: === "..arg1, 1.0, 1.0, 0.0)
 		if (arg1 == "!WhisperBind") then
-			loadvalue();
-			DEFAULT_CHAT_FRAME:AddMessage("WB_storage loaded", 2.0, 0.0, 1.0)
+			DEFAULT_CHAT_FRAME:AddMessage("WhisperBind variables loaded", 1.0, 1.0, 0.0)
+			wbopt = 0;
+			if (wbcount == nil) then wbcount = 3 end
+			
+			WBInterfaceFrame_Resize(wbcount)
+			wbinterface_make_buttons();
+			wbinterface_make_editboxes();
+			WB_loadvalue();
 		end
 	end
 end
 
 
-function ClickButton(player, text)
-	if (string.len(player)>1) and (string.len(text)>1) then
+function WBClickButton(id)
+	--local text = clicked_button:GetText()
+	id = id - 5
+	--DEFAULT_CHAT_FRAME:AddMessage("id = "..id, 1.0, 1.0, 0.0)
+	--DEFAULT_CHAT_FRAME:AddMessage("player = "..WB_playerbox[id], 1.0, 1.0, 0.0)
+	--DEFAULT_CHAT_FRAME:AddMessage("message = "..WB_messagebox[id], 1.0, 1.0, 0.0)
+	
+	local player, text = WB_playerbox[id], WB_messagebox[id]
+	--DEFAULT_CHAT_FRAME:AddMessage("player = "..player, 1.0, 1.0, 0.0)
+	--DEFAULT_CHAT_FRAME:AddMessage("text = "..text, 1.0, 1.0, 0.0)
+	
+	--if (string.len(player)>1) and (string.len(text)>1) then
+	if (string.len(player)>1) then
 		SendChatMessage(text, "WHISPER", nil, player);
 	end
 end
 
-function optionsbtn()
-    if(optvalue == 0) then
-        options:Show()
-        SaveFrame:Show()
-        optvalue = 1;
+
+function WB_optionsbtn()
+    if(wbopt == 0) then
+		--DEFAULT_CHAT_FRAME:AddMessage("show", 1.0, 1.0, 0.0)
+        WBOptionsFrame:Show()
+        --SaveFrame:Show()
+        wbopt = 1;
 
     else
-        options:Hide()
-        SaveFrame:Hide()
-        optvalue = 0;
+		--DEFAULT_CHAT_FRAME:AddMessage("hide", 1.0, 1.0, 0.0)
+        WBOptionsFrame:Hide()
+        --SaveFrame:Hide()
+        wbopt = 0;
 
     end
 end
 
-function savevalue()
 
-	if(namebox1:GetText() ~= nil)   then
-		WB_storage.namebox1 = namebox1:GetText();
-		whisperbindbutton1:SetText(WB_storage.namebox1);
-	end
-	if(namebox2:GetText() ~= nil)   then
-		WB_storage.namebox2 = namebox2:GetText();
-		whisperbindbutton2:SetText(WB_storage.namebox2);
-	end
-	if(namebox3:GetText() ~= nil)   then
-		WB_storage.namebox3 = namebox3:GetText();
-		whisperbindbutton3:SetText(WB_storage.namebox3);
-	end
-	if(namebox4:GetText() ~= nil)   then
-		WB_storage.namebox4 = namebox4:GetText();
-		whisperbindbutton4:SetText(WB_storage.namebox4);
-	end
-	if(namebox5:GetText() ~= nil)   then
-		WB_storage.namebox5 = namebox5:GetText();
-		whisperbindbutton5:SetText(WB_storage.namebox5);
-	end
-	if(namebox6:GetText() ~= nil)   then
-		WB_storage.namebox6 = namebox6:GetText();
-		whisperbindbutton6:SetText(WB_storage.namebox6);
-	end
-	if(namebox7:GetText() ~= nil)   then
-		WB_storage.namebox7 = namebox7:GetText();
-		whisperbindbutton7:SetText(WB_storage.namebox7);
-	end
-	if(namebox8:GetText() ~= nil)   then
-		WB_storage.namebox8 = namebox8:GetText();
-		whisperbindbutton8:SetText(WB_storage.namebox8);
-	end
-	if(namebox9:GetText() ~= nil)   then
-		WB_storage.namebox9 = namebox9:GetText();
-		whisperbindbutton9:SetText(WB_storage.namebox9);
-	end
-	if(namebox10:GetText() ~= nil)   then
-		WB_storage.namebox10 = namebox10:GetText();
-		whisperbindbutton10:SetText(WB_storage.namebox10);
-	end
 
-	WB_storage.playerbox1 = playerbox1:GetText();
-	WB_storage.playerbox2 = playerbox2:GetText();
-	WB_storage.playerbox3 = playerbox3:GetText();
-	WB_storage.playerbox4 = playerbox4:GetText();
-	WB_storage.playerbox5 = playerbox5:GetText();
-	WB_storage.playerbox6 = playerbox6:GetText();
-	WB_storage.playerbox7 = playerbox7:GetText();
-	WB_storage.playerbox8 = playerbox8:GetText();
-	WB_storage.playerbox9 = playerbox9:GetText();
-	WB_storage.playerbox10 = playerbox10:GetText();
+-- Rogolist edition
+function WB_savevalue()
 
-	WB_storage.messagebox1 = messagebox1:GetText();
-	WB_storage.messagebox2 = messagebox2:GetText();
-	WB_storage.messagebox3 = messagebox3:GetText();
-	WB_storage.messagebox4 = messagebox4:GetText();
-	WB_storage.messagebox5 = messagebox5:GetText();
-	WB_storage.messagebox6 = messagebox6:GetText();
-	WB_storage.messagebox7 = messagebox7:GetText();
-	WB_storage.messagebox8 = messagebox8:GetText();
-	WB_storage.messagebox9 = messagebox9:GetText();
-	WB_storage.messagebox10 = messagebox10:GetText();
+	for ii = 1,wbcount do
+		
+
+		WB_buttonbox[ii] = WB_editbox[ii]:GetText()
+		--DEFAULT_CHAT_FRAME:AddMessage("WB_buttonbox["..ii.."] = "..WB_editbox[ii]:GetText(), 1.0, 1.0, 0.0)
+		local b=ii+5
+		WB_button[b]:SetText(WB_buttonbox[ii])
+		
+		local p = ii + wbcount
+		WB_playerbox[ii] = WB_editbox[p]:GetText()
+		--DEFAULT_CHAT_FRAME:AddMessage("WB_playerbox["..ii.."] = "..WB_editbox[p]:GetText(), 1.0, 1.0, 0.0)
+
+		local m = ii + wbcount*2
+		WB_messagebox[ii] = WB_editbox[m]:GetText()
+		--DEFAULT_CHAT_FRAME:AddMessage("WB_mesagebox["..ii.."] = "..WB_editbox[m]:GetText(), 1.0, 1.0, 0.0)
+	end
+	
+
+end
+
+function WB_loadvalue()
+	
+	WB_button[1]:SetText("Options");
+	WB_button[2]:SetText("Save");
+	WB_button[3]:SetText("ButtonID");
+	WB_button[4]:SetText("Player");
+	WB_button[5]:SetText("Message");
+
+	for ii = 1,wbcount do
+		local b = ii+5
+		--WB_button[b]:SetText("Test");
+		WB_button[b]:SetText(WB_buttonbox[ii]);
+		--DEFAULT_CHAT_FRAME:AddMessage("WB_button["..b.."] = "..WB_buttonbox[ii], 1.0, 1.0, 0.0)
+		
+		--WB_editbox[ii]:SetText("Name");
+		WB_editbox[ii]:SetText(WB_buttonbox[ii]);
+		--DEFAULT_CHAT_FRAME:AddMessage("WB_editbox["..ii.."] = "..WB_buttonbox[ii], 1.0, 1.0, 0.0)
+		local p = ii + wbcount
+		--WB_editbox[p]:SetText("Play");
+		WB_editbox[p]:SetText(WB_playerbox[ii]);
+		--DEFAULT_CHAT_FRAME:AddMessage("WB_editbox["..p.."] = "..WB_playerbox[ii], 1.0, 1.0, 0.0)
+		local m = ii + wbcount*2
+		--WB_editbox[m]:SetText("Mess");
+		WB_editbox[m]:SetText(WB_messagebox[ii]);
+		--DEFAULT_CHAT_FRAME:AddMessage("WB_editbox["..m.."] = "..WB_messagebox[ii], 1.0, 1.0, 0.0)
+	end
+end
+
+function WB_debug()
+	local yy =1
+	while WB_button[yy] do
+		WB_button[yy]:SetText("Butt_"..yy);
+		yy = yy + 1
+	end
+	
+	local yy =1
+	while WB_editbox[yy] do
+		WB_editbox[yy]:SetText("EBox_"..yy);
+		yy = yy + 1
+	end
+	
+end
+
+function WBdefaultVariables()
+	for i=1,wbcount do
+		WB_buttonbox[i] = "name"..i
+		WB_playerbox[i] = "play"..i
+		WB_messagebox[i] = "mess"..i
+	end
 end
 
 
-function whisperbind_show()
-	for i,k in pairs(WB_storage) do
+function WB_interface_show()
+	DEFAULT_CHAT_FRAME:AddMessage("BUTTONS:", 1.0, 1.0, 0.0)
+	for i,k in pairs(WB_buttonbox) do
 		if (k ~= nil) then DEFAULT_CHAT_FRAME:AddMessage(i.." - "..k, 1.0, 1.0, 0.0) end
 	end
+	
+	DEFAULT_CHAT_FRAME:AddMessage("PLAYERS:", 1.0, 1.0, 0.0)
+	for i,k in pairs(WB_playerbox) do
+		if (k ~= nil) then DEFAULT_CHAT_FRAME:AddMessage(i.." - "..k, 1.0, 1.0, 0.0) end
+	end
+	
+	DEFAULT_CHAT_FRAME:AddMessage("MESSAGES:", 1.0, 1.0, 0.0)
+	for i,k in pairs(WB_messagebox) do
+		if (k ~= nil) then DEFAULT_CHAT_FRAME:AddMessage(i.." - "..k, 1.0, 1.0, 0.0) end
+	end
+	
 end
 
-function loadvalue()
 
-	whisperbindbutton1:SetText(WB_storage.namebox1);
-	namebox1:SetText(WB_storage.namebox1);
-	whisperbindbutton2:SetText(WB_storage.namebox2);
-	whisperbindbutton3:SetText(WB_storage.namebox3);
-	whisperbindbutton4:SetText(WB_storage.namebox4);
-	whisperbindbutton5:SetText(WB_storage.namebox5);
-	whisperbindbutton6:SetText(WB_storage.namebox6);
-	whisperbindbutton7:SetText(WB_storage.namebox7);
-	whisperbindbutton8:SetText(WB_storage.namebox8);
-	whisperbindbutton9:SetText(WB_storage.namebox9);
-	whisperbindbutton10:SetText(WB_storage.namebox10);
-	
-	namebox1:SetText(WB_storage.namebox1);
-	namebox2:SetText(WB_storage.namebox2);
-	namebox3:SetText(WB_storage.namebox3);
-	namebox4:SetText(WB_storage.namebox4);
-	namebox5:SetText(WB_storage.namebox5);
-	namebox6:SetText(WB_storage.namebox6);
-	namebox7:SetText(WB_storage.namebox7);
-	namebox8:SetText(WB_storage.namebox8);
-	namebox9:SetText(WB_storage.namebox9);
-	namebox10:SetText(WB_storage.namebox10);
-	
-	playerbox1:SetText(WB_storage.playerbox1);
-	playerbox2:SetText(WB_storage.playerbox2);
-	playerbox3:SetText(WB_storage.playerbox3);
-	playerbox4:SetText(WB_storage.playerbox4);
-	playerbox5:SetText(WB_storage.playerbox5);
-	playerbox6:SetText(WB_storage.playerbox6);
-	playerbox7:SetText(WB_storage.playerbox7);
-	playerbox8:SetText(WB_storage.playerbox8);
-	playerbox9:SetText(WB_storage.playerbox9);
-	playerbox10:SetText(WB_storage.playerbox10);
-	
-	messagebox1:SetText(WB_storage.messagebox1);
-	messagebox2:SetText(WB_storage.messagebox2);
-	messagebox3:SetText(WB_storage.messagebox3);
-	messagebox4:SetText(WB_storage.messagebox4);
-	messagebox5:SetText(WB_storage.messagebox5);
-	messagebox6:SetText(WB_storage.messagebox6);
-	messagebox7:SetText(WB_storage.messagebox7);
-	messagebox8:SetText(WB_storage.messagebox8);
-	messagebox9:SetText(WB_storage.messagebox9);
-	messagebox10:SetText(WB_storage.messagebox10);
-end
+
